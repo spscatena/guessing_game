230a7e224c96fa5b8f78a273fc034a987908a4fc
Sabrina Scatena <sabrina.scatena@gmail.com>
2021-02-02 18:14:23 -0500

    guess the image

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+window.onload = init;
+function init() {
+  let images = document.getElementsByTagName("img");
+  for (let i = 0; i < images.length; i++) {
+    images[i].onclick = showAnswer;
+  }
+}
+
+function showAnswer(eventObj) {
+  let image = eventObj.target;
+  let name = image.id;
+  name = name + ".jpg";
+  image.src = name;
+}
