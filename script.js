1cd373fc310332e4384a59cbb87a0aa1f8bd5f81
Sabrina Scatena <sabrina.scatena@gmail.com>
2021-02-03 18:28:18 -0500

    timeout set

--- script.js
+++ script.js
@@ -13,4 +13,12 @@ function showAnswer(eventObj) {
   let name = image.id;
   name = name + ".jpg";
   image.src = name;
+
+  setTimeout(reblur, 2000, image);
+}
+
+function reblur(image) {
+  let name = image.id;
+  name = name + "blur.jpg";
+  image.src = name;
 }
